8e29008d191b540b47ac1060513c3dd9570a3f6b
Dan Parry <parryd424@gmail.com>
2017-04-26 17:52:43 -0400

    Mission complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,54 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+    "Hey"
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  post '/posts' do
+    Post.create(params)
+    @posts = Post.all
+    redirect to('/posts')
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    post = Post.find(params[:id])
+
+
+    if post.update(name: params[:name], content: params[:content])
+      redirect to("/posts/#{post.id}")
+    else
+      erb :show
+    end
+
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @rip = @post.name
+    @post.destroy
+    erb :delete
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+
 end
